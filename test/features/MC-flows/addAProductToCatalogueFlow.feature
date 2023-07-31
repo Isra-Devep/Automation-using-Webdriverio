e03e9999503eb190b4339641cf9f562f7059a351
Ameer <ameer.hussain@1m.app>
2023-07-31 16:31:14 +0500

    changes made to the structure

--- test/features/MC-flows/addAProductToCatalogueFlow.feature
+++ test/features/MC-flows/addAProductToCatalogueFlow.feature
@@ -1,14 +1,14 @@
 @regression @search
-Feature: A user purchases items from a store
+Feature: A user wants to add a product to their store
 
         # Scenario 1:
-        Scenario:  A user signs into the merchant store and adds a prodcut to the catalgoue. 
+        Scenario:  A user signs into the merchant store and adds an active product to the catalogue. 
                 Given User opens the via application and checks he's not signed in
                 When The user tries to login using the given credentials
                 Then Verify the user has signed in
                 When The user opens the Products page from the hamburger menu
                 Then The user will be able to access the products page
-                When The user click the add new item button
+                When The user clicks the add new item button
                 Then The user will be able to see the add product page
                 When The user adds the product info
                 Then The user will be able to add the product to the catalogue
--- test/stepDefinitions/MC-flows/addAProductToCatalogueFlow.steps.ts
+++ test/stepDefinitions/MC-flows/addAProductToCatalogueFlow.steps.ts
@@ -1,13 +1,12 @@
 import { Given, Then, When } from "@cucumber/cucumber";
-import credentials from "../../main/ui/config/signInCredentials.json";
-import * as singInPageActions from "../../main/ui/singInPage/singInPage.tasks";
-
-import * as myStorePageActions from "../../main/ui/myStorePage/myStorePage.tasks";
-import * as myStorePageAssertions from "../../main/ui/myStorePage/myStorePage.assertions";
-import * as productPageActions from "../../main/ui/productsPage/productsPage.tasks";
-import * as productPageAssertions from "../../main/ui/productsPage/productPage.assertions";
-import * as addAProductPageActions from "../../main/ui/addAProductPage/addAProductPage.tasks";
-import * as signInPageAssertions from "../../main/ui/addAProductPage/addAProductPage.assertions"
+import credentials from "../../../main/Test-Data/config/signInCredentials.json";
+import * as singInPageActions from "../../../main/ui/singInPage/singInPage.tasks";
+import * as myStorePageActions from "../../../main/ui/myStorePage/myStorePage.tasks";
+import * as myStorePageAssertions from "../../../main/ui/myStorePage/myStorePage.assertions";
+import * as productPageActions from "../../../main/ui/productsPage/productsPage.tasks";
+import * as productPageAssertions from "../../../main/ui/productsPage/productPage.assertions";
+import * as addAProductPageActions from "../../../main/ui/addAProductPage/addAProductPage.tasks";
+import * as signInPageAssertions from "../../../main/ui/addAProductPage/addAProductPage.assertions"
 
 Given(
     "User opens the via application and checks he's not signed in",
@@ -34,7 +33,7 @@ Then ("The user will be able to access the products page",
 async() =>{
     await productPageAssertions.checkIfProductPageIsOpen(); 
 })
-When ("The user click the add new item button",
+When ("The user clicks the add new item button",
 async() => {
     await productPageActions.addNewProductItem();
 })
