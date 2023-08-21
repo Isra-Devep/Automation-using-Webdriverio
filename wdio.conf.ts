1aca3ce93e015b9bae524a125aad82199023a1ce
Ameer <ameer.hussain@1m.app>
2023-08-21 20:09:18 +0500

    adding cucumber HTML

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -132,7 +132,7 @@ export const config: Options.Testrunner = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['cucumberjs-json'],
+    reporters: [['cucumberjs-json', { outputDir: './reports/json' }]],
 
     //
     // If you are using Cucumber you need to specify the location of your step definitions.
